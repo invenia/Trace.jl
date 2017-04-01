34c560b26fded8502534e8ea47708bc15f4a9d3b
Rory-Finnegan <rory.finnegan@gmail.com>
2017-04-01 12:52:53 -0500

    Initial attempt to get `@trace` working.

--- src/Trace.jl
+++ src/Trace.jl
@@ -1,5 +1,71 @@
 module Trace
 
-# Package code goes here.
+using Memento
+
+global const TRACE_LEVEL = 5
+global const DEFAULT_FMT_STRING = "[{level} | {lookup}]: {msg}"
+
+export @trace
+
+"""
+    @config(args...)
+
+Sets up a "trace" log level and creates a "Trace" logger
+which we'll be using for all `@trace` messages.
+
+# Arguments (order matters)
+* `fmt::AbstractString`: a format string to use (Default=Trace.DEFAULT_FMT_STRING).
+* `io::IO`: an IO type to print to (Default=STDOUT).
+"""
+macro config(args...)
+    fmt = length(args) > 0 ? args[1] : DEFAULT_FMT_STRING
+    io = length(args) > 1 ? args[2] : STDOUT
+
+    quote
+        Memento._log_levels["trace"] = 5
+        Memento._loggers["Trace"] = Logger(
+            "Trace";
+            level="trace",
+            propagate=false
+        )
+        add_handler(
+            Memento._loggers["Trace"],
+            DefaultHandler(
+                $io,
+                DefaultFormatter($fmt),
+            ),
+            "base"
+        )
+
+        Memento._loggers["Trace"]
+    end
+end
+
+"""
+    @trace(msg)
+
+Logs the `msg` to the "Trace" logger.
+"""
+macro trace(msg)
+    if haskey(Memento._loggers, "Trace")
+        logger = Memento._loggers["Trace"]
+        level = logger.level
+        levelnum = logger.levels[level]
+
+        if levelnum <= TRACE_LEVEL
+            return quote
+                dict = Dict{Symbol, Any}(
+                    :name => "Trace",
+                    :level => "trace",
+                    :levelnum => TRACE_LEVEL,
+                    :msg => $msg
+                )
+                log($logger, dict)
+            end
+        end
+    end
+
+    return :nothing
+end
 
 end  # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,14 @@
 using Trace
 using Base.Test
+using Memento
 
-# Write your own tests here.
-@test 1 == 2
+@testset "Basic Usage" begin
+    @trace "My skipped message"
+    @Trace.config(Trace.DEFAULT_FMT_STRING, IOBuffer())
+    @trace "My logged message"
+    # io = get_handlers(get_logger("Trace"))["base"].io
+    # log_str = takebuf_string(io)
+    # println(log_str)
+    # @test contains(log_str, "trace")
+    # @test contains(log_str, "My logged message")
+end
